9a39d5622cd768eaad92857a2e5b876ce99ece87
Iker Garitaonandia <iker.garitaonandia@netcentric.biz>
2019-07-27 15:51:07 +0200

    Adds header menu

--- themes/ikerhexo/src/index.js
+++ themes/ikerhexo/src/index.js
@@ -2,6 +2,7 @@ import ComponentLoader from 'component.loader';
 
 import 'modules/coin-flip.module';
 import 'modules/footer-date.module';
+import 'modules/home-menu.module';
 import 'modules/progress-bar.module';
 import 'modules/search-bar.module';
 

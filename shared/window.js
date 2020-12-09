b0588daa6a05a7f3326378c5d51f294373495d1a
Iker Garitaonandia <iker@garitaonandia.com>
2020-12-09 23:37:28 +0100

    Install and use Draggable :fire:

--- shared/window.js
+++ shared/window.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Draggable from 'react-draggable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
@@ -9,16 +10,20 @@ import styles from 'styles/modules/window.module.scss';
 const Window = (props) => {
   const { children, title } = props;
   return (
-    <div className={styles.container}>
-      <div className={styles.titlebar}>
-        <Link href="/">
-          <button className={styles.close}>
-            <FontAwesomeIcon icon={faTimes} />
-          </button>
-        </Link>
-        <title>{title}</title>
-      </div>
-      {children}
+    <div className={styles.wrapper}>
+      <Draggable handle={`.${styles.title}`}>
+        <div className={styles.container}>
+          <div className={styles.titleBar}>
+            <Link href="/">
+              <button className={styles.close}>
+                <FontAwesomeIcon icon={faTimes} />
+              </button>
+            </Link>
+            <div className={styles.title}>{title}</div>
+          </div>
+          {children}
+        </div>
+      </Draggable>
     </div>
   );
 };

32742f07846f8b7500cd0050a92d574c3c1acd09
Iker Garitaonandia <iker@garitaonandia.com>
2021-09-18 22:39:23 +0200

    Improve head meta tags

--- shared/custom-head.js
+++ shared/custom-head.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import StatusBar from './status-bar';
+import { AppContext } from '../config';
 
-function CustomHead({ title, description }) {
+function CustomHead({
+  author, title, description, keywords,
+}) {
+  const { asPath } = useRouter();
+  const appContext = useContext(AppContext);
+  const { settings } = appContext;
+
   return (
     <React.Fragment>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <title>{title}</title>
         <meta name="description" content={description} />
-        <meta name="keywords" content="<%= keywords %>" />
-        <meta name="author" content="Iker Garitaonandia" />
+        <meta name="keywords" content={keywords.join()} />
+        <meta name="author" content={author} />
 
         <meta property="og:title" content={title} />
         <meta property="og:type" content="article" />
-        <meta property="og:url" content="<%= url %>" />
-        <meta property="og:image" content="<%= image %>" />
+        <meta property="og:url" content={`https://iker.io${asPath}`} />
+        <meta property="og:image" content={settings.gravatar} />
         <meta property="og:description" content={description} />
         <meta property="og:site_name" content={title} />
-        <meta property="article:tag" content="<%= keywords %>" />
+        <meta property="article:tag" content={keywords.join()} />
       </Head>
       <StatusBar title={title} />
     </React.Fragment>
@@ -27,13 +35,17 @@ function CustomHead({ title, description }) {
 }
 
 CustomHead.defaultProps = {
+  author: 'Iker Garitaonandia',
   title: 'Iker Garitaonandia | Software Developer',
   description: 'My name is Iker Garitaonandia and I\'m a professional software developer with 15+ years of experience helping companies achieve digital transformation.',
+  keywords: ['Software developer', 'Engineer', 'Web developer', 'JavaScript', 'Frontend', 'HTML', 'CSS'],
 };
 
 CustomHead.propTypes = {
+  author: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
+  keywords: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default CustomHead;
